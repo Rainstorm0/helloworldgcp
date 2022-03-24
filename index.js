0201d4b8ac0c053a111e5fe4b3bdc36a54292034
Rainstorm0 <jtreagor@gmail.com>
2022-03-24 16:14:16 -0400

    add test page

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send(`Hello ${name}!`);
 });
 
+app.get('/test', (req, res) => {
+  res.send("WHOAAAA");
+});
+
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
